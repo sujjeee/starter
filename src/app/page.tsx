437d44933154acfc2410f290f943b54a72a2520d
Suraj <sujjeee@outlook.com>
2024-09-09 22:48:49 +0530

    update: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import BlurHeroDemo from "@/components/layouts/hero"
-import Image from "next/image"
+import { getGithubStars } from "@/actions/github"
+import { Hero } from "@/components/sections/hero"
 
-export default function Home() {
+export default async function HomePage() {
+  const stars = await getGithubStars()
+
   return (
     <div className="fixed left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
-      <BlurHeroDemo />
+      <Hero stars={stars} />
     </div>
   )
 }
---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,43 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { MoveRight } from "lucide-react"
+
+interface HeroProps {
+  stars: number
+}
+
+export function Hero({ stars }: HeroProps) {
+  return (
+    <div className="flex min-h-screen items-center justify-center p-4">
+      <div className="max-w-screen-sm">
+        <div className="mb-8 flex items-center space-x-6">
+          <Badge
+            variant="outline"
+            className="relative rounded-full px-3 py-1 text-xs leading-6 text-muted-foreground ring-1.5 ring-inset ring-muted-foreground"
+          >
+            <Icons.github className="size-4 mr-1.5" /> {stars} Stars on Github
+          </Badge>
+        </div>
+        <h1 className="mb-6 text-[56px] text-foreground leading-[61px]">
+          Build modern websites with next.js
+        </h1>
+        <p className="mb-8 text-[21px] text-muted-foreground leading-[28px]">
+          Spend your weekend creating amazing web apps with a <br /> collection
+          of popular tech stacks and tools.
+        </p>
+        <div className="flex items-center space-x-6">
+          <Button size={"sm"}>Get started</Button>
+          <a
+            target="_blank"
+            href="https://github.com/sujjeee/starter"
+            className="flex items-center text-sm font-medium text-muted-foreground"
+          >
+            Documentation
+            <MoveRight className="ml-1 size-4" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
