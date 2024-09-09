1ca8a56a3f3525303ac53535ff50f4bd605979c7
Suraj <sujjeee@outlook.com>
2024-09-09 22:47:20 +0530

    add: theme

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from "react"
+import { ThemeProvider } from "./theme"
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  )
+}
