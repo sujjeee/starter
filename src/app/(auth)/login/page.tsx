60566ef51b2057c73677d3c62ba53ecf03c0c79d
Suraj <sujjeee@outlook.com>
2024-09-10 01:10:50 +0530

    update: lucia auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,7 +11,7 @@ export default async function LoginPage() {
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <Command className="mx-auto h-6 w-6" />
+          <Command className="mx-auto size-6" />
           <h1 className="text-2xl font-semibold tracking-tight">
             Welcome back
           </h1>
--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,9 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
     DATABASE_AUTH_TOKEN: z.string(),
+
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_ID_CLIENT_SECRET: z.string(),
   },
 
   client: {
@@ -15,5 +18,8 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
+
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_ID_CLIENT_SECRET: process.env.GOOGLE_CLIENT_ID_CLIENT_SECRET,
   },
 })
